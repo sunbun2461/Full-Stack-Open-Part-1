d0911f26238daffd4c874964dbd27dee88d19839
Thomas Rio <thomasrio@admins-MacBook-Pro.local>
2022-02-15 00:25:18 -0500

    commiting folders/files

---
+++ part1/src/App.js
@@ -0,0 +1,28 @@
+const Hello = (props) => {
+    return (
+        <>
+            <p>Hello {props.name}, you are {props.age} years old.</p>
+           
+        </>
+    )
+}
+
+
+const App = () => {
+
+    const name = "Peter"
+    const age = 10
+
+    return(
+        <>
+            <h1>Gretings</h1>
+            <Hello name="Maya" age={26 + 10} />
+            <Hello name={name} age={age} /> 
+        
+        </>
+    )
+
+
+}
+
+export default App 
