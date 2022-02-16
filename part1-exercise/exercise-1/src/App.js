7820ef00b4cb04cccb999d4fcd1f5d99b4b64eb8
Thomas Rio <thomasrio@admins-MacBook-Pro.local>
2022-02-16 01:17:41 -0500

    Exercise 1.2

--- part1-exercise/exercise-1/src/App.js
+++ part1-exercise/exercise-1/src/App.js
@@ -7,14 +7,23 @@ const Header = (props) => {
     </>
   )
 }
-
-const Content = (props) => {
- 
+const Part = (props) => {
   return (
     <>
       <p>{props.part1} {props.exercises1}</p>
       <p>{props.part2} {props.exercises2}</p>
       <p>{props.part3} {props.exercises3}</p>     
+    </>
+  )
+}
+const Content = (props) => {
+ 
+  return (
+    <>
+    <Part part1={props.part1} exercises1={props.exercises1} />
+    <Part part2={props.part2} exercises2={props.exercises2} />
+    <Part part3={props.part3} exercises3={props.exercises3} />
+     
     </>
   )
 }
